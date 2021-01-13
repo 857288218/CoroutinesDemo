b4e76b135a17ed8b6aa4f5e49650866dca439f18
renjunqing <renjunqing@windimg.com>
2021-01-13 11:54:05 +0800

    demo

--- app/src/main/java/com/example/rjq/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/rjq/myapplication/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.rjq.myapplication
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.*
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.rjq.myapplication.progress.LoadingDialog
 import com.example.rjq.myapplication.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class MainActivity : BaseActivity() {
@@ -45,15 +52,54 @@ class MainActivity : BaseActivity() {
         TestLoader.instance2
 
         lifecycleScope.launch {
+            flow {
+                for (i in 1..10) {
+                    Log.d("rjqflow", "emit $i")
+                    emit(i)
+                }
+            }.catch {
 
+            }.filter {
+                Log.d("rjqflow", "filter $it")
+                true
+            }.map {
+                Log.d("rjqflow", "map $it")
+                it
+            }.flowOn(Dispatchers.IO).onCompletion {
+                Log.d("rjqflow", "完成")
+            }.collect {
+                Log.d("rjqflow", "collect $it")
+            }
+        }
+
+        lifecycleScope.launch {
+            val currentTime = System.currentTimeMillis()
+            val a = async(Dispatchers.IO) {
+                Thread.sleep(1000)
+//                delay(1000)
+                1 + 2
+            }
+
+            val b = async(Dispatchers.IO) {
+                Thread.sleep(2000)
+//                delay(2000)
+                1 + 3
+            }
+
+            val c = a.await() + b.await()
+            //将async中Dispatchers.IO去掉，那么时间是3秒
+            Log.d("rjqasync", "${System.currentTimeMillis() - currentTime}, c: $c")
+        }
+
+        findViewById<View>(R.id.click_me_BN).setOnClickListener {
+            viewModel.login("15620419359", "rjq015")?.observe(this, Observer {
+                result_TV?.text = it?.toString()
+            })
         }
     }
 
     override fun onResume() {
         super.onResume()
-        viewModel.login("15620419359", "rjq015")?.observe(this, Observer {
-            result_TV?.text = it?.toString()
-        })
     }
 
     override fun onDestroy() {
