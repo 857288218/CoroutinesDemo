ac527cc6db7aaab10926bb6a39a6c0d3b98f60e9
857288218 <31814795+857288218@users.noreply.github.com>
2022-05-17 10:19:12 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/rjq/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/rjq/myapplication/MainActivity.kt
@@ -75,7 +75,7 @@ class MainActivity : BaseActivity() {
      */
     // 挂起函数：suspend修饰的函数最终都要调用到suspendCoroutineUninterceptedOrReturn函数,该函数接收一个block参数,该函数作用是将所在协程挂起即该协程中代码暂不执行了,该协程指定的线程就去处理其他任务了,
     // 并执行该函数的block参数 在block中可根据条件调用cont.resume恢复挂起协程继续在该协程指定的线程执行代码，如果不调用cont.resume挂起的协程就永远不会继续执行
-    // 例如withContext(Dispatcher.IO){...}该挂起函数将所在协程挂起并在IO线程中执行block 执行完后调用了cont.resume恢复挂起协程继续执行代码
+    // 例如withContext(Dispatcher.IO){}该挂起函数将所在协程挂起并在suspendCoroutineUninterceptedOrReturn的block中在IO线程执行withContext传入的block 执行完后调用了cont.resume恢复挂起协程继续执行代码
     // 所以挂起协程并不是一直占用协程指定的线程而是挂起协程内的代码暂不执行了不占用该线程了,该线程就去做别的事情了,当调用cont.resume时相当于向挂起的协程所指定的线程postRunnable执行挂起的代码
     private fun testSharedFlow() {
         sharedFlow = MutableSharedFlow<String>(replay = 1, extraBufferCapacity = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
