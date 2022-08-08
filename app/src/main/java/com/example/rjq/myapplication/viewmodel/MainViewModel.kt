03193d43c280eb15d1d5611fe7efcebf6ad834a0
857288218 <31814795+857288218@users.noreply.github.com>
2022-08-08 17:16:54 +0800

    update

--- app/src/main/java/com/example/rjq/myapplication/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/rjq/myapplication/viewmodel/MainViewModel.kt
@@ -30,8 +30,9 @@ class MainViewModel(app: Application) : BaseViewModel(app) {
                 val currentTime = System.currentTimeMillis()
                 Log.d("renjunqingTime", Thread.currentThread().toString())
                 //在viewModelScope/lifecycleScope.launch/async协程代码块里用launch/async再开协程相当于向该"协程post消息"等待执行,只有当viewModelScope/lifecycleScope协程里的代码执行完再回来按顺序执行launch/async协程里代码
-                //viewModelScope/lifecycleScope.launch/async会立即执行代码块里的内容,只有当该协程里的代码执行完(包括里面launch/async代码块)或挂起才会执行协程后的代码
-                //CoroutineScope().launch/async需要等其后面的代码执行完再执行该协程里的代码相当于向线程postMessage,可以使用Dispatchers.Main.immediate立即执行协程里的代码,viewModelScope/lifecycleScope就使用的Dispatchers.Main.immediate
+                //viewModelScope/lifecycleScope.launch/async如果在主线程中调用会立即执行代码块里的内容,只有当该协程里的代码执行完(包括里面launch/async代码块)或挂起才会执行协程后的代码
+                //CoroutineScope().launch/async需要等其后面的代码执行完再执行该协程里的代码相当于向线程postMessage,可以使用Dispatchers.Main.immediate当在主线程上被调用，它不会被重新调度，而是会被立刻执行,viewModelScope/lifecycleScope就使用的Dispatchers.Main.immediate
+                // 参考：https://itcn.blog/p/0804310534.html
 
                 //下面例子先打印System.currentTimeMillis() - currentTime,主线程睡3秒(deffer1.await(),deffer2.await()执行的情况下最后才打印System.currentTimeMillis()-currentTime,主线程睡3秒)，
                 //然后第一个launch/async执行:主线程睡3秒,login(挂起函数)切到子线程中执行,该协程会挂起需要等login执行完成后再执行；如果login不是挂起函数,那么会立即打印"我是第一个login后的代码"
